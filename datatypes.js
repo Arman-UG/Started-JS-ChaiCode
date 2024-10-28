038b0ec5a15b5d28b11acdb493224e65ee1a57a1
Arman-AR <armanarsh123456@gmail.com>
2024-10-28 17:04:50 +0000

    Data types

---
+++ datatypes.js
@@ -0,0 +1,40 @@
+//Generally Datatypes are two type:
+
+// Primitive
+
+// 7 types : String , Number , Boolean , BigInt , null , undefined , Symbol
+
+// const String = "Arman"
+// const Number = 100
+// const Boolean = true or false 
+// const value = null
+// const Score; // undefined
+// const Id = ("123") // symbol
+// const anotherId = ("123") // symbol
+// console.log(Id === anotherid) // output is "false"
+// both value are same but when we check it output must be "false"
+// const nigNum = 243723876326437643874n // bigint
+
+
+// Refrence (non - Primituve)
+
+// Arrays , Object , Function
+
+// Arrays = [] this is array using method
+// const arrays = ["Arman" , "Mukesh" , "Hello"] //anything you can write in arrays even number also
+
+// object = {}  this is the method for using objects inside cerly braces
+// const myObj = {
+    // name : "Arman" ,
+    // age : 36
+// } it can  contain anything string , array , number , boolean , function
+
+
+// function we can use it two way first declare direct function like : function () {} fn contain both () , {}
+// second way decleare through the variable like: const MyFunction = function () {}
+
+// if you want to see any datatype use this:
+// console.log(typeof any variable name)
+// eg ==> // const Number = 100// output  is "number"
+
+// remember null typeof is object 
