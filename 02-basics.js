5f0761f43351968b4090fdf573a1d77b037ad89b
Arman-AR <armanarsh123456@gmail.com>
2024-10-28 13:06:01 +0000

    next topic Datatype

---
+++ 02-basics.js
@@ -0,0 +1,31 @@
+"Use strict"; // treat all JS code as a newer version
+
+alert(3 + 3)// don't use this because we using node Js not browser otherwise on browser it will run
+
+// console.log(3 + 3); console.log(4 + 5) but this is rong way to write a code
+
+// code redability should be high 
+
+let name = "Arman"
+
+let age = 19
+
+// their is many type of datatype but they divided into two cetogry 
+// primitive && non-primitive
+//primitive datatype :-
+
+// number => like 4 , 5 , 6 , and so on they assign 2 to power 53
+// String => ""
+//BigInt => use for big  numbers
+// boolean => true/false
+// null => standlone value
+// undefined => not assign value
+// symbole => unique
+
+
+
+//object
+
+console.log(typeof null)// object
+console.log(typeof undefined) // undefined
+
